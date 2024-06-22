ef4c7fd3f9c05c1fcacf6c78c5d9c6333772dd63
fornicol07 <fornicol07@yahoo.com>
2024-06-22 06:21:21 +0300

    Create SQLQuery3_Utilizare functii sql.sql

---
+++ SQLQuery3_Utilizare functii sql.sql
@@ -0,0 +1,17 @@
+Select concat('google','.com') as ConcatFunction
+
+Select ASCII (ProductID) NumberCodeOfID
+From Products
+
+Select ASCII ('0')
+
+Select CHAR(64) as codeTOCharacter
+
+Select LOWER ('SQL Functions')
+Select UPPER ('SQL Functions')
+Select REPLACE ('TUTORIAL','T','t')
+Select GETDATE()
+Select DAY('2022/10/31') AS DAY
+Select MONTH('2022/10/31') AS month
+Select YEar('2022/10/31') AS year
+
